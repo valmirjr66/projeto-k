1df6e0ec29be865d08e18418b89da13022572f8c
Valmir Junior <valmirgmj@gmail.com>
2021-02-16 20:18:18 -0300

    Commit inicial

---
+++ modules/observador/src/main/java/com/exemplo/iot/ScheduledTask.java
@@ -0,0 +1,47 @@
+package com.exemplo.iot;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.Base64;
+
+import javax.imageio.ImageIO;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import boofcv.abst.scene.ImageClassifier;
+import boofcv.factory.scene.ClassifierAndSource;
+import boofcv.factory.scene.FactoryImageClassifier;
+import boofcv.io.image.ConvertBufferedImage;
+import boofcv.struct.image.GrayF32;
+import boofcv.struct.image.Planar;
+import deepboof.io.DeepBoofDataBaseOps;
+
+@Service
+public class ScheduledTask {
+    @Scheduled(fixedDelay = 300000)
+    public void task() throws IOException {
+        ClassifierAndSource classifierSource = FactoryImageClassifier.nin_imagenet();
+
+        File modelPath = DeepBoofDataBaseOps.downloadModel(classifierSource.getSource(), new File("download_data"));
+
+        ImageClassifier<Planar<GrayF32>> classifier = classifierSource.getClassifier();
+        classifier.loadModel(modelPath);
+
+        byte[] imageByte = Base64.getDecoder().decode(getImage().getBytes());
+        ByteArrayInputStream imageInputStream = new ByteArrayInputStream(imageByte);
+        BufferedImage bufferedImage = ImageIO.read(imageInputStream);
+
+        Planar<GrayF32> resultImage = new Planar<>(GrayF32.class, bufferedImage.getWidth(), bufferedImage.getHeight(),
+                3);
+        ConvertBufferedImage.convertFromPlanar(bufferedImage, resultImage, true, GrayF32.class);
+
+        classifier.classify(resultImage);
+    }
+
+    private String getImage() {
+        return new ImageGetter().getImage();
+    }
+}
